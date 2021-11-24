47bbd1a7a124415274f3d6791050ade418a8768b
Arnaldo Cisneros <arnaldo.jose.cisneros.zambrano@gmail.com>
2021-11-24 00:14:35 -0500

    Finished most of the methods of the Class LinkedList

--- linked_list.py
+++ linked_list.py
@@ -16,6 +16,10 @@ class LinkedList:
 
     def get_element(self, index):
         """Returns the value of the element at the provided index. O(n)"""
+        
+        if self.head == None:
+            raise Exception("Linked list is empty")
+        
         currentNode = self.head
         for i in range(index):
             if currentNode.nextNode == None:
@@ -43,34 +47,90 @@ class LinkedList:
     def pop_front(self):
         """Removes the val from the front of the list and returns the value
         of that val. O(1)"""
-        
-        # currentNode = self.head
-
-        # while True:
-        #     if currentNode.nextNode == None:
-        #         storagedValue = currentNode.value
-        #         currentNode = None
-        #         return storagedValue
-        #     currentNode = currentNode.nextNode
 
         if self.head == None:
             return None
         
-        savedElement = self.head.nextNode
-        self.head = self.head.nextNode.nextNode
+        savedElement = self.head.value
+        
+        self.head = self.head.nextNode
 
-        return savedElement.value
+        return savedElement
 
 
     def insert_after(self, index, val):
         """Inserts an val in the list after the provided index. O(n)"""
-        raise NotImplementedError
+        
+        # HEAD -> A(0) -> B(1) -> C(2) -> D(3)
+        # insert_after(1, 'F')
+        # HEAD -> A(0) -> B(1) -> F(2) -> C(3) -> D(4)
+
+        if self.head == None:
+            raise Exception("Linked list is empty")
+
+        currentNode = self.head
+
+        for i in range(index):
+            currentNode = currentNode.nextNode
+
+        currentNode.nextNode = Node(val, currentNode.nextNode)
 
     def remove_element(self, index):
         """Removes element at the provided index. Returns the removed
         element. O(n)"""
-        raise NotImplementedError
+        
+        if self.head == None:
+            raise Exception("Linked list is empty")
+
+        if index == 0:
+            return self.pop_front()
+
+        # HEAD -> A(0) -> B(1) -> F(2) -> C(3) -> D(4)
+        # remove_element(1)
+        # HEAD -> A(0) -> F(1) -> C(2) -> D(3)
+
+        currentNode = self.head
+
+        for i in range(index):
+            currentNode = currentNode.nextNode
+
+        savedElement = currentNode
+
+        listOfNodes = currentNode.nextNode
+
+        currentNode = self.head
+
+        for i in range(index-1):
+            currentNode = currentNode.nextNode
+
+        currentNode.nextNode = listOfNodes
+
+        return savedElement.value
 
     def reverse(self):
         """Reverses the direction of the linked list. O(n)"""
-        raise NotImplementedError
+        
+        if self.head == None:
+            raise Exception("Linked list is empty")
+
+        if self.count == 1:
+            return
+
+        # HEAD -> A(0) -> B(1) -> C(2) -> D(3) -> E(4) -> F(5)
+        # reverse()
+        # HEAD -> F(0) -> E(1) -> D(2) -> C(3) -> B(4) -> A(5)
+
+        # currentNode = self.head
+
+        # tail = None
+
+        # for i in range(self.count()):
+            
+        #     aux = currentNode
+        #     aux.nextNode = tail
+        #     tail = aux
+
+        #     if currentNode.nextNode != None:
+        #         currentNode = currentNode.nextNode
+
+        # self.head = tail
--- test_linked_list.py
+++ test_linked_list.py
@@ -1,9 +1,13 @@
-from linked_list import LinkedList, Node
+from linked_list import LinkedList
 import pytest
+
 
 def test_get_element():
     
     linked_list = LinkedList()
+
+    with pytest.raises(Exception, match='Linked list is empty'):
+        linked_list.get_element(0)
 
     linked_list.push_front('a')
     linked_list.push_front('b')
@@ -12,9 +16,6 @@ def test_get_element():
     assert linked_list.get_element(0) == 'c'
     assert linked_list.get_element(1) == 'b'
     assert linked_list.get_element(2) == 'a'
-
-
-    #HEAD -> C -> B -> A 
 
 
 def test_get_element_index_too_large():
@@ -30,6 +31,7 @@ def test_get_element_index_too_large():
 
     with pytest.raises(Exception, match='index too large'):
         linked_list.get_element(10)
+
 
 def test_count():
 
@@ -54,14 +56,156 @@ def test_pop_front():
     
     linked_list = LinkedList()
 
+    assert linked_list.pop_front() == None
+
+    linked_list.push_front('a')
+
+    assert linked_list.pop_front() == 'a'
+    
+    with pytest.raises(Exception, match='Linked list is empty'):
+        linked_list.get_element(0)
+
     linked_list.push_front('a')
     linked_list.push_front('b')
     linked_list.push_front('c')
 
     assert linked_list.get_element(0) == 'c'
-
-    
+    assert linked_list.get_element(1) == 'b'
+    assert linked_list.get_element(2) == 'a'
 
     assert linked_list.pop_front() == 'c'
+    assert linked_list.get_element(0) == 'b'
+    assert linked_list.get_element(1) == 'a'
 
-    #assert linked_list.get_element(0) == 'b'
+
+def test_insert_after():
+    
+    linked_list = LinkedList()
+
+    with pytest.raises(Exception, match='Linked list is empty'):
+        linked_list.insert_after(0, 'f')
+
+    linked_list.push_front('a')
+    linked_list.push_front('b')
+    linked_list.push_front('c')
+
+    assert linked_list.get_element(0) == 'c'
+    assert linked_list.get_element(1) == 'b'
+    assert linked_list.get_element(2) == 'a'
+
+    # HEAD -> C -> B -> A
+
+    linked_list.insert_after(1, 'f')
+
+    # HEAD -> C -> B -> F -> A
+
+    assert linked_list.get_element(0) == 'c'
+    assert linked_list.get_element(1) == 'b'
+    assert linked_list.get_element(2) == 'f'
+    assert linked_list.get_element(3) == 'a'
+
+    linked_list_2 = LinkedList()
+
+    linked_list_2.push_front('a')
+
+    linked_list_2.insert_after(0, 'b')
+
+    # HEAD -> A -> B
+
+    assert linked_list_2.get_element(0) == 'a'
+    assert linked_list_2.get_element(1) == 'b'
+    
+    linked_list_2.insert_after(1, 'c')
+
+    assert linked_list_2.get_element(0) == 'a'
+    assert linked_list_2.get_element(1) == 'b'
+    assert linked_list_2.get_element(2) == 'c'
+
+
+def test_remove_element():
+
+    linked_list = LinkedList()
+
+    with pytest.raises(Exception, match='Linked list is empty'):
+        linked_list.remove_element(1)
+
+    linked_list.push_front('a')
+    linked_list.push_front('b')
+    linked_list.push_front('c')
+    linked_list.push_front('d')
+    linked_list.push_front('e')
+
+    # HEAD -> E -> D -> C -> B -> A
+
+    assert linked_list.get_element(0) == 'e'
+    assert linked_list.get_element(1) == 'd'
+    assert linked_list.get_element(2) == 'c'
+    assert linked_list.get_element(3) == 'b'
+    assert linked_list.get_element(4) == 'a'
+
+    #REMOVING ELEMENT IN THE MIDDLE OF THE LIST
+    assert linked_list.remove_element(1) == 'd'
+
+    # HEAD -> E -> C -> B -> A
+
+    assert linked_list.get_element(0) == 'e'
+    assert linked_list.get_element(1) == 'c'
+    assert linked_list.get_element(2) == 'b'
+    assert linked_list.get_element(3) == 'a'
+
+    
+    #REMOVING ELEMENT AT THE BEGGINING OF THE LIST
+    assert linked_list.remove_element(0) == 'e'
+
+    # HEAD -> C -> B -> A
+
+    assert linked_list.get_element(0) == 'c'
+    assert linked_list.get_element(1) == 'b'
+    assert linked_list.get_element(2) == 'a'
+
+    
+    #REMOVING ELEMENT AT THE END OF THE LIST
+    assert linked_list.remove_element(2) == 'a'
+
+    # HEAD -> C -> B
+
+    assert linked_list.get_element(0) == 'c'
+    assert linked_list.get_element(1) == 'b'
+
+
+def test_reverse():
+
+    linked_list = LinkedList()
+
+    with pytest.raises(Exception, match='Linked list is empty'):
+        linked_list.remove_element(1)
+
+    linked_list.push_front('a')
+    linked_list.push_front('b')
+    linked_list.push_front('c')
+    linked_list.push_front('d')
+    linked_list.push_front('e')
+
+    assert linked_list.get_element(0) == 'e'
+    assert linked_list.get_element(1) == 'd'
+    assert linked_list.get_element(2) == 'c'
+    assert linked_list.get_element(3) == 'b'
+    assert linked_list.get_element(4) == 'a'
+
+    # HEAD -> E -> D -> C -> B -> A
+
+    linked_list.reverse()
+
+    #assert linked_list.get_element(0) == 'e'
+    #assert linked_list.get_element(1) == 'e'
+    #assert linked_list.get_element(2) == 'e'
+    #assert linked_list.get_element(3) == 'e'
+    #assert linked_list.get_element(4) == 'e'
+
+    #assert linked_list.get_element(0) == 'a'
+    #assert linked_list.get_element(1) == 'b'
+    #assert linked_list.get_element(2) == 'c'
+    #assert linked_list.get_element(3) == 'd'
+    #assert linked_list.get_element(4) == 'e'
+
+    # HEAD -> A -> B -> C -> D -> E
